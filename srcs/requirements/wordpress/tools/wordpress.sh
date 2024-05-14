651e2289684a6d585d74bb1e2440a76f314b3166
cwenz <wenzconrad@gmail.com>
2024-05-14 18:11:53 +0200

    wip - wordpress does not find content for some reason

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -13,12 +13,12 @@ echo db pasword: $WP_DB_PASSWORD
 
 # Configuring wp-config.php
 wp config create --dbname="$WP_DB_NAME" --dbuser="$WP_USERNAME" \
-	--dbpass="$WP_DB_PASSWORD" --dbhost=mariadb:3306 --allow-root
+	--dbpass="$WP_DB_PASSWORD" --dbhost=mariadb:3306
 
 # Install WordPress
-wp core install --url="https://localhost" --title="My Wordpress site" \
+wp core install --url="https://localhost" --title="Ahhhhhh help me" \
 	--admin_user="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASSWORD" \
-	--admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root
+	--admin_email="$WP_ADMIN_EMAIL" --skip-email
 
 wp option update home 'https://localhost'
 wp option update siteurl 'https://localhost'
@@ -27,7 +27,9 @@ wp option update siteurl 'https://localhost'
 
 # WP_DB_ROOT_PASSWORD:
 # wp user create $WP_ADMIN_NAME $WP_ADMIN_EMAIL --role=author --user_pass=$WP_ADMIN_PASSWORD
-
+chown -R nobody:nobody /var/www/html
+find . -type d -exec chmod 755 {} \;
+find . -type f -exec chmod 644 {} \;
 # Start PHP-FPM
 # exec php-fpm82 -F
 # exec "$@"
