af0f8b5a8c7ec13cbc42d04f12c82a974434039a
cwenz <wenzconrad@gmail.com>
2024-05-21 13:18:02 +0200

    fixed env variables in mariadb script

--- srcs/requirements/mariadb/tools/startup_mariadb.sh
+++ srcs/requirements/mariadb/tools/startup_mariadb.sh
@@ -3,6 +3,10 @@
 # Path where MariaDB stores its data
 DATA_PATH="/var/lib/mysql"
 
+# echo "DB_NAME: " $DB_NAME
+# echo "DB_USERNAME: " $DB_USERNAME
+# echo "DB_PASSWORD: " $DB_PASSWORD
+
 rc-service mariadb start
 
 while ! mysqladmin ping --silent; do
@@ -12,9 +16,9 @@ done
 
 mariadb --user=root << EOF
 FLUSH PRIVILEGES;
-CREATE DATABASE IF NOT EXISTS ${DB_NAME};
-CREATE USER IF NOT EXISTS '${DB_USERNAME}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USERNAME}'@'%';
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USERNAME'@'%';
 FLUSH PRIVILEGES;
 EOF
 
